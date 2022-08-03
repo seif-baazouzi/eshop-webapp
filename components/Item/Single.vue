e9d1e6d94a220714c85d892c3b91fe03a4c7a73c
seif <baazouziseif9@gmail.com>
2022-08-03 10:24:38 +0100

    single item component

---
+++ components/Item/Single.vue
@@ -0,0 +1,91 @@
+<template>
+  <div class="container">
+    <img :src="apiServer + '/images/' + image" alt="">
+    <div class="content">
+      <Rate :value="rate" />
+      <h1>{{ name }}</h1>
+      <div class="price">{{ formatPrice(price) }} dt</div>
+      <div class="description">
+        <h5>Description:</h5>
+        <p>{{ description }}</p>
+      </div>
+    </div>
+  </div>
+</template>
+
+<script lang="ts">
+import { apiServer } from "../../config/config"
+import formatPrice from "../../utils/format-price"
+
+export default {
+  props: {
+    name: {
+      type: String,
+      required: true
+    },
+    image: {
+      type: String,
+      required: true
+    },
+    description: {
+      type: String,
+      required: true
+    },
+    price: {
+      type: Number,
+      required: true
+    },
+    rate: {
+      type: Number,
+      required: true
+    },
+  },
+  
+  created() {
+    this.apiServer = apiServer
+    this.formatPrice = formatPrice
+  },
+}
+</script>
+
+<style scoped>
+  .content {
+    padding-top: 1rem;
+  }
+
+  h1 {
+    line-height: 1;
+  }
+
+  .price {
+    font-size: .75rem;
+    font-weight: bold;
+    color: var(--dark-gray);
+  }
+
+  .description {
+    margin-top: 2rem;
+  }
+
+  .description p {
+    font-size: .75rem;
+    color: var(--dark-gray);
+  }
+
+  @media screen and (min-width: 768px) {
+    .container {
+      display: flex;
+      align-items: flex-start;
+      gap: 1rem;
+    }
+
+    img {
+      width: 60%;
+      border-radius: .5rem;
+    }
+
+    .content {
+      width: 40%;
+    }
+  }
+</style>
--- pages/items/_itemID.vue
+++ pages/items/_itemID.vue
@@ -1,24 +1,18 @@
 <template>
   <div>
     <NavBar/>
-    <div class="container">
-      <img :src="apiServer + '/images/' + item.itemImage" alt="">
-      <div class="content">
-        <Rate :value="item.rate" />
-        <h1>{{ item.itemName }}</h1>
-        <div class="price">{{ formatPrice(item.itemPrice) }} dt</div>
-        <div class="description">
-          <h5>Description:</h5>
-          <p>{{ item.itemDescription }}</p>
-        </div>
-      </div>
-    </div>
+    <ItemSingle
+      :name="item.itemName"
+      :image="item.itemImage"
+      :rate="item.rate"
+      :price="item.itemPrice"
+      :description="item.itemDescription"
+    />
   </div>
 </template>
 
 <script lang="ts">
 import { apiServer } from "../../config/config"
-import formatPrice from "../../utils/format-price"
 
 export default {
   name: "Item",
@@ -31,7 +25,6 @@ export default {
 
   created() {
     this.apiServer = apiServer
-    this.formatPrice = formatPrice
   },
 
   async fetch() {    
@@ -42,45 +35,3 @@ export default {
   },
 }
 </script>
-
-<style scoped>
-  .content {
-    padding-top: 1rem;
-  }
-
-  h1 {
-    line-height: 1;
-  }
-
-  .price {
-    font-size: .75rem;
-    font-weight: bold;
-    color: var(--dark-gray);
-  }
-
-  .description {
-    margin-top: 2rem;
-  }
-
-  .description p {
-    font-size: .75rem;
-    color: var(--dark-gray);
-  }
-
-  @media screen and (min-width: 768px) {
-    .container {
-      display: flex;
-      align-items: flex-start;
-      gap: 1rem;
-    }
-
-    img {
-      width: 60%;
-      border-radius: .5rem;
-    }
-
-    .content {
-      width: 40%;
-    }
-  }
-</style>
