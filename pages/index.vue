21c0936066cc4af5cfa99e550c34dd44a7361a57
seif <baazouziseif9@gmail.com>
2022-08-15 10:33:15 +0100

    improve home page design

--- pages/index.vue
+++ pages/index.vue
@@ -2,11 +2,7 @@
   <div>
     <NavBar />
 
-    <div class="welcome">
-      <h1>Welcome to our e-shop platform</h1>
-    </div>
-
-    <div class="list-container">
+    <div class="list-container" v-if="items.length > 0">
       <ItemList :items="items" title="Newest Items" :seeMore="true" />
     </div>
 
@@ -53,34 +49,3 @@ export default {
 }
 
 </script>
-
-<style scoped>
-  .welcome {
-    width: 100%;
-    min-height: 50vh;
-    margin-bottom: 1rem;
-    position: relative;
-    display: flex;
-    align-items: center;
-    justify-content: center;
-    background-size: cover;
-    background-position: center;
-    background-image: url("/images/bg.jpg");
-  }
-
-  .welcome::before {
-    content: "";
-    width: 100%;
-    height: 100%;
-    position: absolute;
-    top: 0; left: 0;
-    background-color: rgba(0, 0, 0, .45);
-  }
-
-  .welcome h1 {
-    color: white;
-    position: relative;
-    text-align: center;
-    text-shadow: var(--shadow);
-  }
-</style>
